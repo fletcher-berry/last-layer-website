ba58fc59b14104fc859e519bee6df377502d3ea0
fletcher-berry <32848772+fletcher-berry@users.noreply.github.com>
2018-12-02 17:40:51 -0600

    Adding project - home page done

---
+++ Last_layer_website_dotnet/Last_layer_website_dotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Last_layer_website_dotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
